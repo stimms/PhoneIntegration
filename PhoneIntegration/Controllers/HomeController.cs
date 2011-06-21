b559e63a852ed1acfe8e49f2521d8f46e46d672b
stimms <stimms@gmail.com>
2011-06-20 19:01:27 -0600

    Initial add

---
+++ PhoneIntegration/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneIntegration.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Phone Box";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
